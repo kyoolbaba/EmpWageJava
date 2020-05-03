957d02112f2946843d24767aa2e5cb54b16f4bfc
kyoolbaba <kyoolbaba@gmail.com>
2020-05-03 06:48:24 +0530

    Added feature of Part time and calculating its DailyWage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,16 +1,23 @@
 public class EmployeeWage{
 public static void main(String[] args){
 	//CONSTANTS
-	int IS_FULL_TIME=1;
+	final int IS_PART_TIME=1;
+	final int IS_FULL_TIME=2;
 	int EMP_RATE_PER_HOUR=20;
 	//variables
 	int empHrs=0;
 	int empWage=0;
 	//computations
-	double empCheck=Math.floor(Math.random()*10%2);
-	if(empCheck==IS_FULL_TIME)
+	int empCheck=(int)Math.floor(Math.random()*10%3);
+	switch (empCheck){
+	case IS_PART_TIME:
+		empHrs=4;
+		break;
+	case IS_FULL_TIME:
 		empHrs=8;
-		empWage=empHrs*EMP_RATE_PER_HOUR;
-		System.out.println("Emp Wage : "+empWage);
+		break;
 	}
+	empWage=empHrs*EMP_RATE_PER_HOUR;
+	System.out.println("The Employee Wage is "+empWage);
+}
 }
