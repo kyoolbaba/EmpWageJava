8b63ab6d4abe07d48c68d16f46f44b987f76e3d3
kyoolbaba <kyoolbaba@gmail.com>
2020-05-03 06:01:44 +0530

    Added feature for Calculating EmployeeWage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,10 +1,16 @@
 public class EmployeeWage{
 public static void main(String[] args){
+	//CONSTANTS
 	int IS_FULL_TIME=1;
+	int EMP_RATE_PER_HOUR=20;
+	//variables
+	int empHrs=0;
+	int empWage=0;
+	//computations
 	double empCheck=Math.floor(Math.random()*10%2);
 	if(empCheck==IS_FULL_TIME)
-		System.out.println("Employee is present");
-	else
-		System.out.println("Employee is absent");
-}
+		empHrs=8;
+		empWage=empHrs*EMP_RATE_PER_HOUR;
+		System.out.println("Emp Wage : "+empWage);
+	}
 }
