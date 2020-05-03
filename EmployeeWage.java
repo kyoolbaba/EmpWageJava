dc33428b6db549a6c58a0f9ab08cef9015af1d5e
kyoolbaba <kyoolbaba@gmail.com>
2020-05-03 09:15:37 +0530

    Added Feature for calculating Wages for a month

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -4,10 +4,13 @@ public static void main(String[] args){
 	final int IS_PART_TIME=1;
 	final int IS_FULL_TIME=2;
 	int EMP_RATE_PER_HOUR=20;
+	int NO_OF_DAYS=20;
 	//variables
 	int empHrs=0;
 	int empWage=0;
+	int totalWage=0;
 	//computations
+	for(int day=0;day<NO_OF_DAYS;day++){
 	int empCheck=(int)Math.floor(Math.random()*10%3);
 	switch (empCheck){
 	case IS_PART_TIME:
@@ -16,8 +19,13 @@ public static void main(String[] args){
 	case IS_FULL_TIME:
 		empHrs=8;
 		break;
+	default:
+	empHrs=0;
 	}
 	empWage=empHrs*EMP_RATE_PER_HOUR;
+	totalWage+=empWage;
 	System.out.println("The Employee Wage is "+empWage);
+	System.out.println("The Total Wage is "+totalWage);
+}
 }
 }
